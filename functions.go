b47ae078f5dca0065f77f80d35965751bbd7eb32
Devin <dvillega@gmail.com>
2012-11-05 20:10:19 -0600

    Pointers

--- functions.go
+++ functions.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func plus (a int, b int) int {
-    return a + b
+func plus(a int, b int) int {
+	return a + b
 }
 
 func main() {
-    res := plus(1,2)
-    fmt.Println("1 + 2 =", res)
+	res := plus(1, 2)
+	fmt.Println("1 + 2 =", res)
 }
--- varaidic.go
+++ varaidic.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-func sum (nums ...int) {
-    fmt.Print(nums, " ")
-    total := 0
-    for _, num := range nums {
-        total += num 
-    }
-    fmt.Println(total)
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
 }
 
 func main() {
-    sum(1,2)
-    sum(1,2,3)
+	sum(1, 2)
+	sum(1, 2, 3)
 
-    nums := []int{1,2,3,4,5,6}
-    sum(nums...)
+	nums := []int{1, 2, 3, 4, 5, 6}
+	sum(nums...)
 }
